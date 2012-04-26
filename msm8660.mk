0778f8bcea2d039d502e367ebcdbb0132181c4e4
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-26 08:58:06 -0400

    Update notations to specify library origins correctly

--- msm8660.mk
+++ msm8660.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit msm8660 Vendor Files
+# Inherit Qualcomm prebuilt libraries
 $(call inherit-product-if-exists, vendor/qcom/qcom-vendor-blobs.mk)
 
 # usb profiles
